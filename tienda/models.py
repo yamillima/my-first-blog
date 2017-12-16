1855f71f718df3b81919effa5600ed6c5dbc195d
Yamil <yamilandreslima@gmail.com>
2017-12-15 23:33:06 -0500

    strings de fechas y nombres en models y cambios en templates

--- tienda/models.py
+++ tienda/models.py
@@ -13,17 +13,32 @@ class Producto(models.Model):
     abierto_desde_las = models.TimeField()
     cierra_a_las = models.TimeField()
 
+    def __str__(self):
+        return self.nombre
+
     def precio_millar(self):
         return "{:,}".format(self.precio).replace(',','.')
 
 class Visita(models.Model):
     fecha = models.DateTimeField(default=datetime.datetime.now())
 
+    def __str__(self):
+        return timezone.localtime(self.fecha).strftime('%b %d de %Y - %I:%M:%S %p')
+
 class Dama(models.Model):
     fecha = models.DateTimeField(default=datetime.datetime.now())
+
+    def __str__(self):
+        return timezone.localtime(self.fecha).strftime('%b %d de %Y - %I:%M:%S %p')
 
 class Hombre(models.Model):
     fecha = models.DateTimeField(default=datetime.datetime.now())
 
+    def __str__(self):
+        return timezone.localtime(self.fecha).strftime('%b %d de %Y - %I:%M:%S %p')
+
 class Chico(models.Model):
     fecha = models.DateTimeField(default=datetime.datetime.now())
+
+    def __str__(self):
+        return timezone.localtime(self.fecha).strftime('%b %d de %Y - %I:%M:%S %p')
